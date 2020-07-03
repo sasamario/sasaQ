31cdfa3a4dd0beeb30a10ee8c3ffb0a466418398
sasamario <mametaku3@yahoo.co.jp>
2020-07-04 00:53:23 +0900

    Fix：質問投稿フォームのレイアウト修正（仮）

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+<form action="#" method="post">
+  {{ csrf_field() }}
+  <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
+  <input type="text" class="form-control m-1" placeholder="プログラミング技術に関するタグをスペース区切りで3つまで入力" name="tags">
+  <div class="row justify-content-center mx-1">
+      <div class="col-6 p-0">
+          <div class="card">
+            <div class="card-header">
+              本文
             </div>
+            <textarea class="card-body p-1" name="article" id="markdown_editor_textarea" cols="30" rows="15" class="form-control"></textarea>
+          </div>
+      </div>
+      <div class="col-6 p-0">
+        <div class="card">
+          <div class="card-header">
+              プレビュー
+          </div>
+          <div class="card-body p-1" id="markdown_preview"></div>
         </div>
-    </div>
-</div>
+      </div>
+  </div>
+  <div class="post-page-footer">
+      <input type="submit" class="post-button m-1" value="Qiitaに投稿">
+  </div>
+</form>
 @endsection

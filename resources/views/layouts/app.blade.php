a8f7d3f2cac9b300dd2a159d770e9c4220030f8d
sasamario <mametaku3@yahoo.co.jp>
2020-07-04 18:48:45 +0900

    Add：投稿用のナビゲーション追加 Add：ドロップダウンメニューの追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,12 +49,16 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nab-item">
+                                <a class="nav-link" href="{{route('create')}}">投稿</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">マイページ</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">

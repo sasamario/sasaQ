39b41feb736e772ebe69e1a1c182abf2e031671f
sasamario <mametaku3@yahoo.co.jp>
2020-07-05 20:51:37 +0900

    Fix：全ての記事を表示するよう修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="thread">
-  
+<div class="thread content justify-content-center">
+    @foreach ($articles as $item)
+        <div class="card">
+            <div class="card-header">
+                <a href="{{route('show', ['id' => $item->article_id])}}">{{$item->title}}</a>
+            </div>
+            <div class="card-body">
+                {{$item->body}}
+            </div>
+        </div>
+    @endforeach
 </div>
 @endsection

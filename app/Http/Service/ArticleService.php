6e5ad30983e7aa411442de299fd4cef1afa0a106
sasamario <mametaku3@yahoo.co.jp>
2020-07-05 20:50:57 +0900

    Add：日付が新しい順で記事データを取得する処理の実装 Add：指定IDの記事データ取得処理の実装

--- app/Http/Service/ArticleService.php
+++ app/Http/Service/ArticleService.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Service;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Article;
 use Illuminate\Support\Facades\Auth;
 
 class ArticleService
 {
-    public function addArticle(Request $request)
+    /**
+     * @param Request $request
+     */
+    public function addArticle(Request $request): void
     {
         //explode関数でスペースを区切り文字として分割する
         $tags = explode(' ', $request->tags);
@@ -25,5 +29,22 @@ class ArticleService
             'body' => $request->body,
             'date' =>now(),
         ]);
+    }
+
+    /**
+     * @return Collection
+     */
+    public function readArticle(): Collection
+    {
+        return Article::orderBy('date', 'desc')->get();
+    }
+
+    /**
+     * @param int $id
+     * @return Article
+     */
+    public function showArticle(int $id): Article
+    {
+        return Article::find($id);
     }
 }

22547e46068d8b754978b73213f4f5f64980fee4
sasamario <mametaku3@yahoo.co.jp>
2020-07-05 20:49:45 +0900

    Add：ホームで記事一覧を表示する処理の実装 Add：特定の記事を表示する機能の実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,14 +7,24 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    /**
+     * @var ArticleService 
+     */
     private $articleService;
 
+    /**
+     * ArticleController constructor.
+     * @param ArticleService $articleService
+     */
     public function __construct(ArticleService $articleService)
     {
         $this->articleService = $articleService;
         $this->middleware('auth');
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('sasaQ.create');
@@ -25,5 +35,26 @@ class ArticleController extends Controller
         $this->articleService->addArticle($request);
 
         return redirect()->route('home');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function read()
+    {
+        $articles = $this->articleService->readArticle();
+
+        return view('home', compact('articles'));
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(int $id)
+    {
+        $article = $this->articleService->showArticle($id);
+
+        return view('sasaQ.article', compact('article'));
     }
 }

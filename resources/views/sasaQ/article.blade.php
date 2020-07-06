210deeba1124153806745ce278f76ff9e0b1864d
sasamario <mametaku3@yahoo.co.jp>
2020-07-06 16:34:18 +0900

    Fix：記事ページに表示する項目の修正

--- resources/views/sasaQ/article.blade.php
+++ resources/views/sasaQ/article.blade.php
@@ -1,12 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            {{$article->title}}
-        </div>
-        <div class="card-body">
-            {{$article->body}}
+    <div class="top-wrapper content mx-auto py-3">
+        <div class="article col-11 mx-auto py-3">
+            <div class="article-header">
+                <div class="article-date">
+                    {{$article->date}}
+                </div>
+                <div class="title">
+                    {{$article->title}}
+                </div>
+                <div class="tags">
+                    <div class="tag">
+                        {{$article->tag1}}
+                    </div>
+                    @if ($article->tag2)
+                        <div class="tag">
+                            {{$article->tag2}}
+                        </div>
+                    @endif
+
+                    @if ($article->tag3)
+                        <div class="tag">
+                            {{$article->tag3}}
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="article-body">
+                {{$article->body}}
+            </div>
         </div>
     </div>
 @endsection

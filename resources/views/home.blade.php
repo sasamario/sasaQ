be66ff2e933ce19ea530df8200297f3f41a6b988
sasamario <mametaku3@yahoo.co.jp>
2020-07-06 19:00:56 +0900

    Fix：細かいデザインの修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="top-wrapper content mx-auto" style="width:60%;">
+<div class="top-wrapper content mx-auto py-3">
     @foreach ($articles as $item)
-        <div class="article-box col-10 mx-auto py-3">
+        <div class="article-box col-11 mx-auto py-3">
             <div class="article-title">
                 <a class="title" href="{{route('show', ['id' => $item->article_id])}}">{{$item->title}}</a>
             </div>

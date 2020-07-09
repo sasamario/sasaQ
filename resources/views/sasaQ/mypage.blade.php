bd5c32b2b6090e0191e62c4e7962023dd374f468
sasamario <mametaku3@yahoo.co.jp>
2020-07-09 22:52:11 +0900

    Add：ドロップボタン追加

--- resources/views/sasaQ/mypage.blade.php
+++ resources/views/sasaQ/mypage.blade.php
@@ -7,6 +7,13 @@
             <div class="article-box col-11 mx-auto py-3">
                 <div class="article-title">
                     <a class="title" href="{{route('show', ['id' => $item->article_id])}}">{{$item->title}}</a>
+                    <div class="article-title-right float-right dropdown" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </div>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="mypage-dropdown-item " href="#">編集 <i class="far fa-edit"></i></a>
+                        <a class="mypage-dropdown-item" href="#">削除 <i class="far fa-trash-alt"></i></a>
+                    </div>
                 </div>
                 <div class="article-date">
                     {{$item->date}}

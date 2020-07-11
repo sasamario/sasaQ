9dc5975a1c77fe733adf43dfb6fc2a5282f1c10d
sasamario <mametaku3@yahoo.co.jp>
2020-07-11 22:15:19 +0900

    Remove：編集・削除ボタンを取り除いた Fix：リンク先の修正
    マイページの記事一覧ではなく、記事ページで編集・削除ボタンを表示させるため

--- resources/views/sasaQ/mypage.blade.php
+++ resources/views/sasaQ/mypage.blade.php
@@ -6,22 +6,7 @@
         @foreach ($myArticles as $item)
             <div class="article-box col-11 mx-auto py-3">
                 <div class="article-title">
-                    <a class="title" href="{{route('show', ['id' => $item->article_id])}}">{{$item->title}}</a>
-                    <div class="article-title-right float-right dropdown" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-h"></i>
-                    </div>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <form action="{{route('edit')}}" method="post" class="mypage-form">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="article_id" value="{{$item->article_id}}">
-                            <button type="submit" class="mypage-form-button">編集 <i class="far fa-edit"></i></button>
-                        </form>
-                        <form action="#" method="post" class="mypage-form">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="article_id" value="{{$item->article_id}}">
-                            <button type="submit" class="mypage-form-button">削除 <i class="far fa-trash-alt"></i></button>
-                        </form>
-                    </div>
+                    <a class="title" href="{{route('myarticle', ['id' => $item->article_id])}}">{{$item->title}}</a>
                 </div>
                 <div class="article-date">
                     {{$item->date}}

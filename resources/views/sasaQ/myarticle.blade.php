fa8ae0bafa86b5e5c2f45741c87ea326d2aa13f7
sasamario <mametaku3@yahoo.co.jp>
2020-07-12 17:54:35 +0900

    Add：削除時のモーダル実装 Fix：モーダル実装に当たって削除ボタンの修正

--- resources/views/sasaQ/myarticle.blade.php
+++ resources/views/sasaQ/myarticle.blade.php
@@ -4,21 +4,47 @@
     <div class="top-wrapper content mx-auto py-3">
         <div class="article col-11 mx-auto py-3">
             <div class="article-header">
+
+                <!-- モーダルの設定 -->
+                <!-- tabindex="-1"によってEscキーでモーダルを閉じれるようにする -->
+                <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModalLabel">削除確認</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p>記事タイトル：「{{$article->title}}」を削除しますか？</p>
+                            </div>
+                            <div class="modal-footer">
+                                <!-- data-dismiss="modal"によって、プラグインを用いてモーダルを閉じている -->
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                                <form action="{{route('delete')}}" method="post" class="myarticle-form">
+                                    {{ csrf_field() }}
+                                    <input type="hidden" name="article_id" value="{{$article->article_id}}">
+                                    <button type="submit" class="btn btn-primary">削除 <i class="far fa-trash-alt"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- ドロップダウンメニュー -->
                 <div class="article-title-right float-right dropdown" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-ellipsis-h"></i>
                 </div>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <form action="{{route('edit')}}" method="post" class="mypage-form">
+                    <form action="{{route('edit')}}" method="post" class="myarticle-form">
                         {{ csrf_field() }}
                         <input type="hidden" name="article_id" value="{{$article->article_id}}">
-                        <button type="submit" class="mypage-form-button">編集 <i class="far fa-edit"></i></button>
+                        <button type="submit" class="myarticle-form-button">編集 <i class="far fa-edit"></i></button>
                     </form>
-                    <form action="{{route('delete')}}" method="post" class="mypage-form">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="article_id" value="{{$article->article_id}}">
-                        <button type="submit" class="mypage-form-button">削除 <i class="far fa-trash-alt"></i></button>
-                    </form>
+                    <button type="button" class="myarticle-form-button" data-toggle="modal" data-target="#deleteModal">削除 <i class="far fa-trash-alt"></i></button>
                 </div>
+
                 <div class="article-date">
                     {{$article->date}}
                 </div>

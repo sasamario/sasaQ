1cd007d69bab4c58f53b795e735f253da2df6018
sasamario <mametaku3@yahoo.co.jp>
2020-07-14 15:54:55 +0900

    Add：ユーザー情報を表示する欄の追加

--- resources/views/sasaQ/mypage.blade.php
+++ resources/views/sasaQ/mypage.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="top-wrapper content mx-auto py-3">
+    <div class="top-wrapper user-wrapper">
+        <div class="user-box col-11 mx-auto">
+            <div class="user-right-box float-left col-5">
+                <div class="user-icon">
+                    <i class="fas fa-user fa-5x"></i>
+                </div>
+                <p class="user-name">〇〇さん</p>
+            </div>
+            <div class="user-info float-right col-7">
+                <p class="article-count">記事投稿数：{{$count}}</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="top-wrapper">
         <p class="my-article-title">今までの投稿記事</p>
         @foreach ($myArticles as $item)
             <div class="article-box col-11 mx-auto py-3">

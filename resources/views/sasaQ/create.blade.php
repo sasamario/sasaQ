63755ae6b0cab99ce583677c591569bacc96f6f6
sasamario <mametaku3@yahoo.co.jp>
2020-07-14 19:17:47 +0900

    Add：old関数の追加
    バリデーション時、直前の入力値を表示するため実装

--- resources/views/sasaQ/create.blade.php
+++ resources/views/sasaQ/create.blade.php
@@ -5,15 +5,15 @@
 @section('content')
 <form action="{{route('add')}}" method="post">
   {{ csrf_field() }}
-  <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
-  <input type="text" class="form-control m-1" placeholder="プログラミング技術に関するタグをスペース区切りで3つまで入力" name="tags">
+  <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title" value="{{ old('title') }}">
+  <input type="text" class="form-control m-1" placeholder="プログラミング技術に関するタグをスペース区切りで3つまで入力" name="tags" value="{{ old('tags') }}">
   <div class="row justify-content-center mx-1">
       <div class="col-6 p-0">
           <div class="card">
             <div class="card-header">
               本文
             </div>
-            <textarea class="card-body p-1" name="body" id="markdown_editor_textarea" cols="30" rows="15" class="form-control"></textarea>
+            <textarea class="card-body p-1" name="body" id="markdown_editor_textarea" cols="30" rows="15" class="form-control">{{ old('body') }}</textarea>
           </div>
       </div>
       <div class="col-6 p-0">

bebcadaca21398b34481ae4f26bfd4fed04a175f
sasamario <mametaku3@yahoo.co.jp>
2020-07-16 00:21:02 +0900

    Add：ReplyControllerの追加 Add：返信フォームでの入力データ保存の実行処理の追加

---
+++ app/Http/Controllers/ReplyController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Service\ReplyService;
+
+class ReplyController extends Controller
+{
+    /**
+     * @var
+     */
+    private $replyService;
+
+    /**
+     * ReplyController constructor.
+     * @param ReplyService $replyService
+     */
+    public function __construct(ReplyService $replyService)
+    {
+        $this->replyService = $replyService;
+        $this->middleware('auth');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * Todo:バリデーション要設定！！！
+     */
+    public function add(Request $request)
+    {
+        $this->replyService->addReply($request);
+
+        return redirect()->route('home');
+    }
+}

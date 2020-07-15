2806c09a771b98a7843baf2d0c4c7c4ff1ec3b14
sasamario <mametaku3@yahoo.co.jp>
2020-07-16 00:22:49 +0900

    Add：replyモデルとのリレーション処理の追加

--- app/Article.php
+++ app/Article.php
@@ -9,4 +9,10 @@ class Article extends Model
   protected $primaryKey = 'article_id';
 
   protected $fillable = ['user_id', 'title', 'tag1', 'tag2', 'tag3', 'body', 'date'];
+
+  public function replies()
+  {
+      //Articleモデルでは、主キーをidではなくarticle_idと設定している。親モデルの主キー名がidでない場合、第三引数で指定する必要がある！
+      return $this->hasMany('App\Reply', 'article_id', 'article_id');
+  }
 }

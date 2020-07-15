52e5f9f5edeacf070f937e57f7de42c74f9d4925
sasamario <mametaku3@yahoo.co.jp>
2020-07-16 00:17:57 +0900

    Add：create_replies_tabelの追加

---
+++ database/migrations/2020_07_15_082919_create_replies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('reply_id');
+            $table->integer('user_id');
+            $table->integer('article_id');
+            $table->text('body');
+            $table->dateTime('reply_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}

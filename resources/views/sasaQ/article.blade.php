bcbcd83b23576136a29a7cbcf4a1928503ea4710
sasamario <mametaku3@yahoo.co.jp>
2020-07-15 17:56:21 +0900

    Add：返信フォームの追加

--- resources/views/sasaQ/article.blade.php
+++ resources/views/sasaQ/article.blade.php
@@ -32,3 +32,18 @@
         </div>
     </div>
 @endsection
+
+@section('reply-form')
+    <div class="reply-wrapper">
+        <p class="form-title">返信フォーム</p>
+        <div class="reply-form">
+            <form action="#" method="post">
+                {{ csrf_field() }}
+                <textarea class="reply-body" style="width:100%;" name="body" cols="30" rows="3" placeholder="テキストを入力"></textarea>
+                <div class="text-right">
+                    <input class="reply-button" type="submit" value="返信">
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection

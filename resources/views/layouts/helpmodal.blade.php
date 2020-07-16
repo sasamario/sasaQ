8f0dd828a57e057c71b70cef56635aa8274134e6
sasamario <mametaku3@yahoo.co.jp>
2020-07-16 17:48:00 +0900

    Fix：ヘルプ用モーダルの文修正

--- resources/views/layouts/helpmodal.blade.php
+++ resources/views/layouts/helpmodal.blade.php
@@ -4,32 +4,54 @@
         <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="helpModalLongTitle">マークダウン記法の詳しい説明</h5>
+                    <h5 class="modal-title" id="helpModalLongTitle">マークダウン記法早見表</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    ※注意！「#」の後ろは半角スペース１つ入れること！<br/>
-                    <strong>【見出し】</strong><br/>
+                    ※注意！マーク(#,-,>等)の後ろは半角スペース１つ入れること！<br/>
+                    <p><strong>【見出し】</strong><br/>
                     # 見出し１<br/>
                     ## 見出し２<br/>
-                    ### 見出し３<br/>
-                    <strong>【リスト】</strong><br/>
+                    ### 見出し３</p>
+
+                    <p><strong>【リスト】</strong><br/>
                     [ - ]、[ + ]、[ * ]のいずれかで箇条書きリストを記述可能<br/>
                     - リスト１<br/>
                     　- ネスト リスト1_1<br/>
-                    　　- ネスト リスト1_1_1<br/>
-                    <strong>【番号付きリスト】</strong><br/>
+                    　　- ネスト リスト1_1_1</p>
+
+                    <p><strong>【番号付きリスト】</strong><br/>
                     1. 番号付きリスト１<br/>
                     　1.番号付きリスト1_1<br/>
-                    　1.番号付きリスト1_2<br/>
-                    <strong>【引用】</strong><br/>
-                    > 引用文<br/>
-                    <strong>【二重引用】</strong><br/>
-                    >> 引用文<br/>
-                    <strong>【code記法】</strong><br/>
-                    `
+                        　1.番号付きリスト1_2</p>
+
+                    <p><strong>【引用】</strong><br/>
+                        例）> 引用文</p>
+
+                    <p><strong>【二重引用】</strong><br/>
+                        例）>> 引用文</p>
+
+                    <p><strong>【code記法】</strong><br/>
+                        例）`code`</p>
+
+                    <p><strong>【強調:em】</strong><br/>
+                    [ * ]or[ _ ]で囲うと強調され、見た目は斜体になります<br/>
+                        例）*強調*</p>
+
+                    <p><strong>【強調:strong】</strong><br/>
+                    [ ** ]or[ __ ]で囲うと強調され、見た目は太字になります<br/>
+                        例）**強調**</p>
+
+                    <p><strong>【強調:em + strong】</strong><br/>
+                    [ *** ]or[ ___ ]で囲うと強調され、見た目は斜体かつ太字になります<br/>
+                        例）***強調***</p>
+
+                    <p><strong>【水平線】</strong><br/>
+                    [ --- ]or[ ___ ]or[ *** ]と３つ以上の連続で記述すると水平線になります<br/>
+                        例）---</p>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>

ac57127d1380cf67424c6f5df10caaa13585b72d
sasamario <mametaku3@yahoo.co.jp>
2020-07-21 23:46:42 +0900

    Fix：レイアウトの修正
    レスポンシブ対応するため

--- resources/views/sasaQ/mypage.blade.php
+++ resources/views/sasaQ/mypage.blade.php
@@ -4,7 +4,8 @@
 
 @section('content')
     <div class="mypage-wrapper row">
-        <div class="mypage-left-wrapper col-7">
+        <!-- タブレットサイズ以上であればmd-7:md-4 それ以下の画面サイズでは1:1 -->
+        <div class="mypage-left-wrapper mx-auto col-10 col-md-7">
             <div class="mypage-article-wrapper">
                 <p class="my-article-title">今までの投稿記事</p>
                 @foreach ($myArticles as $item)
@@ -20,13 +21,13 @@
             </div>
         </div>
 
-        <div class="mypage-right-wrapper col-4">
-            <div class="user-box col-11 mx-auto clearfix">
+        <div class="mypage-right-wrapper mx-auto col-10 col-md-4">
+            <div class="user-box mx-auto clearfix">
                 <div class="user-left-box float-left col-5">
                     <div class="user-icon">
                         <i class="fas fa-user fa-3x"></i>
                     </div>
-                    <p class="user-name">{{ Auth::user()->name }}さん</p>
+                    <p class="user-name">{{ Auth::user()->name }}様</p>
                 </div>
                 <div class="user-right-box float-right col-5">
                     <p class="article-count">投稿数：{{$myArticlesCount}}</p>
@@ -35,8 +36,15 @@
             </div>
 
             <div class="replies-box">
-
+                @foreach ($myReplies as $reply)
+                    <div class="reply-box col-11 mx-auto py-3">
+                        <div class="reply-time">{{$reply->created_at}}</div>
+                        <div class="reply-body">
+                            {{$reply->body}}
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
-    </div>d
+    </div>
 @endsection

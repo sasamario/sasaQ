282d4fd06e910f89197df8b6ebfc7acf36a98efb
sasamario <mametaku3@yahoo.co.jp>
2020-07-22 17:11:32 +0900

    Fix：マイページのレイアウトの修正 Add：ページネーションのボタン追加

--- resources/views/sasaQ/mypage.blade.php
+++ resources/views/sasaQ/mypage.blade.php
@@ -6,8 +6,21 @@
     <div class="mypage-wrapper row">
         <!-- タブレットサイズ以上であればmd-7:md-4 それ以下の画面サイズでは1:1 -->
         <div class="mypage-left-wrapper mx-auto col-10 col-md-7">
+            <div class="user-box mx-auto clearfix">
+                <div class="user-left-box float-left col-5">
+                    <div class="user-icon">
+                        <i class="fas fa-user fa-3x"></i>
+                    </div>
+                    <p class="user-name">{{ Auth::user()->name }}様</p>
+                </div>
+                <div class="user-right-box float-right col-5">
+                    <p class="article-count">総投稿数：{{$myArticlesCount}}</p>
+                    <p class="reply-count">総返信数：{{$myRepliesCount}}</p>
+                </div>
+            </div>
+
             <div class="mypage-article-wrapper">
-                <p class="my-article-title">今までの投稿記事</p>
+                <p class="articles-title">今までの投稿記事</p>
                 @foreach ($myArticles as $item)
                     <div class="article-box col-11 mx-auto py-3">
                         <div class="article-title">
@@ -22,20 +35,8 @@
         </div>
 
         <div class="mypage-right-wrapper mx-auto col-10 col-md-4">
-            <div class="user-box mx-auto clearfix">
-                <div class="user-left-box float-left col-5">
-                    <div class="user-icon">
-                        <i class="fas fa-user fa-3x"></i>
-                    </div>
-                    <p class="user-name">{{ Auth::user()->name }}様</p>
-                </div>
-                <div class="user-right-box float-right col-5">
-                    <p class="article-count">投稿数：{{$myArticlesCount}}</p>
-                    <p class="reply-count">返信数：{{$myRepliesCount}}</p>
-                </div>
-            </div>
-
             <div class="replies-box">
+                <p class="replies-title">今までのコメント</p>
                 @foreach ($myReplies as $reply)
                     <div class="reply-box col-11 mx-auto py-3">
                         <div class="reply-time">{{$reply->created_at}}</div>
@@ -44,6 +45,9 @@
                         </div>
                     </div>
                 @endforeach
+                <div class="replies-paginate row justify-content-center">
+                    {{ $myReplies->links() }}
+                </div>
             </div>
         </div>
     </div>

58676a137210dfc5f89e8570056da5661edf5879
sasamario <mametaku3@yahoo.co.jp>
2020-07-26 21:16:50 +0900

    Add：ReplyRequestの追加
    返信フォームでのバリデーションを行うため

---
+++ app/Http/Requests/ReplyRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReplyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'body' => 'required',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'body.required' => '本文は必ず入力してください',
+        ];
+    }
+}

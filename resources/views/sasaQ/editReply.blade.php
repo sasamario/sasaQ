546cd9dfd83d82265a4911ef0dde6a17838b74ee
sasamario <mametaku3@yahoo.co.jp>
2020-08-01 21:43:40 +0900

    Add：コメント編集フォームの追加

---
+++ resources/views/sasaQ/editReply.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('reply-form')
+    <div class="reply-wrapper">
+        <p class="form-title">返信コメント編集フォーム</p>
+        <div class="reply-form">
+            <form action="{{route('updateReply')}}" method="post">
+                @csrf
+                <textarea class="reply-body" name="body" cols="30" rows="3" placeholder="テキストを入力">{{$editReply->body}}</textarea>
+                <div class="text-right">
+                    <input type="hidden" name="reply_id" value="{{$editReply->reply_id}}">
+                    <input class="reply-button" type="submit" value="返信">
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection

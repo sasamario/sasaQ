3d7f23d3b1dfc9fbb2c0172336e35b1ad27f6f87
sasamario <mametaku3@yahoo.co.jp>
2020-08-01 20:58:48 +0900

    Add：返信コメント編集ページへのルートの追加

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,10 @@ Route::get('/mypage', 'UserController@showMyPage')->name('mypage');
 Route::get('/mypage/article/{id}', 'ArticleController@showMyPageArticle')->name('myarticle');
 
 //編集ページへのルート
-Route::post('/mypage/edit', 'ArticleController@edit')->name('edit');
+Route::post('/mypage/article/edit', 'ArticleController@edit')->name('edit');
 
 //編集ページで入力に誤りがあった場合のリダイレクト時のルート　バリデーション実行後、getでリダイレクトされるため
-Route::get('/mypage/edit', function() {
+Route::get('/mypage/article/edit', function() {
    return redirect()->route('mypage')->with('editErrorMessage', 'タイトル・タグ・本文は必ず入力してください（編集は完了していません）');
 });
 
@@ -55,3 +55,6 @@ Route::post('/mypage/delete', 'ArticleController@delete')->name('delete');
 
 //検索時のルート
 Route::get('/search', 'ArticleController@search')->name('search');
+
+//返信コメントの編集ページへのルート
+Route::post('/mypage/reply/edit', 'ReplyController@edit')->name('editReply');

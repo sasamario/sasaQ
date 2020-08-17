1fc02b80bf12f596017dc4aac4462f1d8acc09fc
sasamario <mametaku3@yahoo.co.jp>
2020-08-17 23:54:44 +0900

    Add：add_column_twitter_id_table.phpの追加
    usersテーブルにtwitter_idカラムを追加するため

---
+++ database/migrations/2020_08_14_231818_add_column_twitter_id_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnTwitterIdTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('twitter_id')->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users　', function (Blueprint $table) {
+            //
+        });
+    }
+}

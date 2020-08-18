a33a3ec27b6312dfaf16ff3893afcaa962e3eb71
sasamario <mametaku3@yahoo.co.jp>
2020-08-18 14:42:40 +0900

    Add：TwitterController.phpの追加

---
+++ app/Http/Controllers/Auth/TwitterController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class TwitterController extends Controller
+{
+    //Twitterの認証ページへユーザーをリダイレクト
+    public function redirectToProvider()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    //ログイン
+    public function handleProviderCallback()
+    {
+        try {
+            //認証結果の受け取り
+            $user = Socialite::driver('twitter')->user();
+        } catch (Exception $e) {
+            return redirect('/');
+        }
+
+        $authUser = $this->findOrCreateUser($user);
+        Auth::login($authUser, true);
+
+        return redirect()->route('home');
+    }
+
+    //Twitterユーザー情報をもとに、ユーザー情報を取得するか新たにユーザーを作成する
+    public function findOrCreateUser($twitterUser)
+    {
+        $authUser = User::where('twitter_id', $twitterUser->id)->first();
+
+        if ($authUser) {
+            return $authUser;
+        }
+
+        //DBにユーザー情報がなければ作成する
+        return User::create([
+            'name' => $twitterUser->nickname,
+            'twitter_id' => $twitterUser->id,
+            'avatar' => $twitterUser->avatar_original,
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}

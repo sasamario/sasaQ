a4602bd929a03a02cb05f453234733eada0fc2a8
sasamario <mametaku3@yahoo.co.jp>
2020-09-02 00:32:37 +0900

    Add：BookmarkComponent.vueの追加

---
+++ resources/js/components/BookmarkComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <button v-on:click="addOrDelete">
+            {{ bookmark }}
+            <i :class="[isActiveTrue === true ? 'fas fa-bookmark' : 'far fa-bookmark']"></i>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookmarkComponent",
+
+    //propsでPHPから送られてきたデータを受け取っている ここでの変数は、v-bindで指定した変数（ケバブケース）のものをスネークケースにする
+    props: ["articleId", "isBookmark"],
+    data() {
+        return {
+            isActiveTrue: this.isActiveTrue = this.isBookmark
+        };
+    },
+    computed: {
+      bookmark: function() {
+          return this.isActiveTrue ? 'ブックマーク済み' : 'ブックマーク'
+      }
+    },
+    methods: {
+        //真偽値の反転
+        change() {
+            this.isActiveTrue = !this.isActiveTrue;
+        },
+        add() {
+            axios
+                .post("/addBookmark", {
+                    articleId: this.articleId,
+                })
+        },
+        delete() {
+            axios
+                .post("/deleteBookmark", {
+                    articleId: this.articleId,
+                })
+        },
+        addOrDelete() {
+            const isTrue = this.isActiveTrue
+            if (isTrue == true) {
+                this.delete();
+                this.change();
+            } else {
+                this.add();
+                this.change();
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

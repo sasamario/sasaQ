2f266b79024bc77417bb0a83ddf94a48188a424f
sasamario <mametaku3@yahoo.co.jp>
2020-09-03 16:07:26 +0900

    Add：BookmarkComponentとbookmark-componentタグの紐付け処理の追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -48,7 +48,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//コンポーネントとタグの紐付け
+Vue.component('bookmark-component', require('./components/BookmarkComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -56,6 +57,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
+new Vue({
     el: '#app',
-});
+})

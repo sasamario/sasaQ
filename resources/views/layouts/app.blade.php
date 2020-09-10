29cc44eda7486c4e3d34057ab8127ec55bcb67a4
sasamario <mametaku3@yahoo.co.jp>
2020-09-10 14:41:54 +0900

    Fix：ドロップダウンメニューのルートの修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{route('mypage')}}">マイページ</a>
                                     <a class="dropdown-item" href="{{route('myBookmark')}}">ブックマーク一覧</a>
-                                    <a class="dropdown-item" href="#">下書き記事一覧</a>
+                                    <a class="dropdown-item" href="{{route('draft')}}">下書き記事一覧</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">

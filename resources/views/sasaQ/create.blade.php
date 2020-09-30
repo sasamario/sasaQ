5fd64d72d47c53cef1d956f65132bc174816e21c
sasamario <mametaku3@yahoo.co.jp>
2020-09-30 17:56:21 +0900

    Add：重要度を指定するためのラジオボタンの追加

--- resources/views/sasaQ/create.blade.php
+++ resources/views/sasaQ/create.blade.php
@@ -7,6 +7,18 @@
 @section('content')
 <form action="{{route('add')}}" method="post">
     @csrf
+    <div class="importance-form form-group">
+        <label>
+            <input type="radio" name="importance" value="{{ \App\Article::STATUS_NOT_HURRY }}" checked>お手すきに
+        </label>
+        <label>
+            <input type="radio" name="importance" value="{{ \App\Article::STATUS_HURRY }}">急ぎ
+        </label>
+        <label>
+            <input type="radio" name="importance" value="{{ \App\Article::STATUS_OTHER }}">その他
+        </label>
+    </div>
+
     <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル（必須）" name="title" value="{{ old('title') }}">
     <input type="text" class="form-control m-1" placeholder="タグを半角スペース区切りで3つまで入力（最低１つ必須）" name="tags" value="{{ old('tags') }}">
     <div class="row justify-content-center mx-1">

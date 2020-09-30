51cd7b31b7e65c5935d9bd94c57fb72399294a24
sasamario <mametaku3@yahoo.co.jp>
2020-09-30 17:58:35 +0900

    Add：データベースに追加するカラムの設定 Add：重要度に関する定数の追加

--- app/Article.php
+++ app/Article.php
@@ -8,13 +8,22 @@ class Article extends Model
 {
   protected $primaryKey = 'article_id';
 
-  protected $fillable = ['user_id', 'title', 'tag1', 'tag2', 'tag3', 'body', 'date', 'status'];
+  protected $fillable = ['user_id', 'title', 'tag1', 'tag2', 'tag3', 'body', 'date', 'status', 'importance'];
 
   //下書き
   const STATUS_DRAFT = 0;
 
   //投稿
   const STATUS_POST = 1;
+
+  //お手すき
+  const STATUS_NOT_HURRY = 0;
+
+  //急ぎ
+  const STATUS_HURRY = 1;
+
+  //その他
+  const STATUS_OTHER = 2;
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany

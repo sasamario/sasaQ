2f6e13eace865fd6fe9002b40833197027e3103e
sasamario <mametaku3@yahoo.co.jp>
2020-09-30 17:59:48 +0900

    Add：Slackへの通知文に重要度を追加

--- app/Notifications/Slack.php
+++ app/Notifications/Slack.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Article;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\SlackMessage;
@@ -16,19 +17,21 @@ class Slack extends Notification
     public $name;
     public $title;
     public $id;
+    public $status;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct(string $name, string $title, int $id)
+    public function __construct(string $name, string $title, int $id, int $status)
     {
         $this->channel = config('app.SLACK_CHANNEL');
         $this->slackName = config('app.SLACK_NAME');
         $this->name = $name;
         $this->title = $title;
         $this->id = $id;
+        $this->status = $status;
     }
 
     /**
@@ -58,8 +61,24 @@ class Slack extends Notification
                $attachment->fields([
                        'タイトル' => $this->title,
                        '投稿者' => $this->name,
-                       'リンク' => $url
+                       'リンク' => $url,
+                       '重要度' => $this->getStatus($this->status)
                    ]);
             });
+    }
+
+    /**
+     * @param int $status
+     * @return string
+     */
+    public function getStatus(int $status)
+    {
+        if ($status == Article::STATUS_NOT_HURRY) {
+            return 'お手すきに';
+        } elseif ($status == Article::STATUS_HURRY) {
+            return 'お急ぎ';
+        } else {
+            return 'その他';
+        }
     }
 }

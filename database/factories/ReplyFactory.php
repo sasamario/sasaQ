c5fbf76c2b93930e46f214a3a78c0d18156975ef
sasamario <mametaku3@yahoo.co.jp>
2020-10-16 17:14:12 +0900

    Fix：タイポの修正

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Reply::class, function (Faker $faker) {
     return [
         'user_id' => $faker->randomNumber(),
-        'article_id' => $faker->randomuNumber(),
+        'article_id' => $faker->randomNumber(),
         'body' => $faker->text(),
     ];
 });

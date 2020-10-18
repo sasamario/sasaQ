892864eeab18b319ea26efd5524ead5440848e84
sasamario <mametaku3@yahoo.co.jp>
2020-10-18 13:34:14 +0900

    Add：ゲストログインボタンの追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -66,6 +66,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
+                                <button type="submit" class="btn btn-secondary guest-btn">
+                                    <a href="/login/guest">Guest Login</a>
+                                </button>
                                 <button type="submit" class="btn twitter-btn">
                                     <a href="/auth/twitter"><i class="fab fa-twitter"></i> Twitter Register</a>
                                 </button>

1270cc0c3ffbcdf526ada822568f63fa8df35b5e
sasamario <mametaku3@yahoo.co.jp>
2020-11-17 11:38:04 +0900

    Fix：コメント投稿フォームに編集ボタンとプレビューボタンが表示されるよう修正

--- resources/views/sasaQ/article.blade.php
+++ resources/views/sasaQ/article.blade.php
@@ -66,7 +66,16 @@
         <div class="reply-form">
             <form action="{{route('addReply')}}" method="post">
                 @csrf
-                <textarea class="reply-body" name="body" cols="30" rows="3" placeholder="テキストを入力"></textarea>
+                <div class="tab-wrap">
+                    <input id="tab-1" type="radio" name="tab" class="tab-switch" checked="checked"><label class="tab-label" for="tab-1">編集</label>
+                    <div class="tab-content">
+                        <textarea class="reply-body" name="body" cols="30" rows="5" placeholder="テキストを入力"></textarea>
+                    </div>
+                    <input id="tab-2" type="radio" name="tab" class="tab-switch preview-check-button"><label class="tab-label" for="tab-2">プレビュー</label>
+                    <div class="tab-content">
+                        <div class="reply-body-preview reply-markdown"></div>
+                    </div>
+                </div>
                 <div class="text-right">
                     <input type="hidden" name="article_id" value="{{$article->article_id}}">
                     <input class="reply-button" type="submit" value="返信">

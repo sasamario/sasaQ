1733a533540722cd629ac51f3c04965346e1a00c
sasamario <mametaku3@yahoo.co.jp>
2020-11-18 19:07:36 +0900

    Fix：app.jsのjQueryの処理部分が処理後と関数化出来ていなかったため修正
    一つにまとめて書いてしまっていたため、処理が動かなくなるなどの予期せぬエラーがあった。処理は関数化すること。

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ require('./bootstrap');
 
 import marked from 'marked';
 
-// マークダウンをプレビュー画面に表示する
+// マークダウンをプレビュー画面に表示する処理
 $(function() {
     marked.setOptions({
         langPrefix: '', // code要素にdefaultで付くlangage-を削除 codeタグのクラス名の接頭辞に関するオプション
@@ -20,6 +20,14 @@ $(function() {
         }
     });
 
+    $('#markdown_editor_textarea').keyup(function() { //keyup()イベントは、押されたキーを話すとイベント発生
+        let html = marked(getHtml($(this).val())); //入力された値をval()で取得　marked()でマークダウン文字列をHTMLタグに変換
+        $('#markdown_preview').html(html); //markdown_previewのhtml要素の書き換え
+    });
+});
+
+//テンプレート作成機能
+$(function() {
     //テンプレート文章を表示する
     $('#template').click(function() {
         const templateBody = (
@@ -39,39 +47,40 @@ $(function() {
         let markedTemplate = marked(templateBody);
         $('#markdown_preview').html($('#markdown_preview').html() + markedTemplate);
     });
+});
 
-    $('#markdown_editor_textarea').keyup(function() { //keyup()イベントは、押されたキーを話すとイベント発生
-        let html = marked(getHtml($(this).val())); //入力された値をval()で取得　marked()でマークダウン文字列をHTMLタグに変換
-        $('#markdown_preview').html(html); //markdown_previewのhtml要素の書き換え
+// 個別の記事画面のマークダウン文字列をHTMLタグに変換する処理
+$(function() {
+    let target = $('.article-body');
+    let html = marked(getHtml(target.html()));
+    target.html(html);
+});
+
+//コメント欄のコメントのマークダウン文字列をHTMLタグに変換する処理
+$(function() {
+    $('.reply-comment').each(function(index, element) {
+        let replyComment = $(element).html();
+        let replyCommentHtml = marked(getHtml(replyComment));
+        $(element).html(replyCommentHtml);
     });
+});
 
-    //プレビューボタンクリック時の処理
+//プレビュー表示処理
+$(function() {
     $('.preview-check-button').click(function() {
         let replyBody = $('.reply-body');
         let replyBodyHtml = marked(getHtml(replyBody.val()));
         $('.reply-body-preview').html(replyBodyHtml);
     });
-
-    // 個別の記事画面のマークダウン文字列をHTMLタグに変換する
-    let target = $('.article-body');
-    let html = marked(getHtml(target.html()));
-    target.html(html);
-
-    //コメント欄のコメントのマークダウン文字列をHTMLタグに変換
-    $('.reply-comment').each(function(index, element) {
-       let replyComment = $(element).html();
-       let replyCommentHtml = marked(getHtml(replyComment));
-       $(element).html(replyCommentHtml);
-    });
-
-    // HTMLでは、比較演算子が &lt; 等になるのでreplace関数で置換を行う　
-    function getHtml(html) {
-        html = html.replace(/&lt;/g, '<');
-        html = html.replace(/&gt;/g, '>');
-        html = html.replace(/&amp;/g, '&');
-        return html;
-    }
 });
+
+// HTMLでは、比較演算子が &lt; 等になるのでreplace関数で置換を行う　
+function getHtml(html) {
+    html = html.replace(/&lt;/g, '<');
+    html = html.replace(/&gt;/g, '>');
+    html = html.replace(/&amp;/g, '&');
+    return html;
+}
 
 window.Vue = require('vue');
 

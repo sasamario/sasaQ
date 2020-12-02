479099675f8829885f9c95845ca160630b94b788
sasamario <mametaku3@yahoo.co.jp>
2020-12-02 10:18:36 +0900

    Add：画像選択ボタンの追加 Add：textareaにクラス名の追加

--- resources/views/sasaQ/editReply.blade.php
+++ resources/views/sasaQ/editReply.blade.php
@@ -9,10 +9,16 @@
             <form action="{{route('updateReply')}}" method="post">
                 @csrf
                 <div class="tab-wrap">
+                    <div class="image-button">
+                        <label for="image">
+                            <i class="far fa-image" data-toggle="tooltip" title="画像を選択"></i>
+                            <input type="file" name="image" id="image" class="input-file-button">
+                        </label>
+                    </div>
                     <div class="help-button" data-toggle="modal" data-target="#helpModal"><i class="far fa-question-circle" data-toggle="tooltip" title="マークダウン記法確認"></i></div>
                     <input id="tab-1" type="radio" name="tab" class="tab-switch" checked="checked"><label class="tab-label" for="tab-1">編集</label>
                     <div class="tab-content">
-                        <textarea class="reply-body" name="body" cols="30" rows="5" placeholder="テキストを入力">{{$editReply->body}}</textarea>
+                        <textarea class="reply-body textarea-image" name="body" cols="30" rows="5" placeholder="テキストを入力">{{$editReply->body}}</textarea>
                     </div>
                     <input id="tab-2" type="radio" name="tab" class="tab-switch preview-check-button"><label class="tab-label" for="tab-2">プレビュー</label>
                     <div class="tab-content">

e5c641b43079d6b0c6aa6f602582e65824d37118
sasamario <mametaku3@yahoo.co.jp>
2020-12-06 21:57:15 +0900

    Fix：トップページのデザインの修正（仮）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,23 +28,20 @@
                 </div>
             @endif
         </div>
-        <div class="welcome-content">
-            <div class="welcome-title">
-                Welcome to <strong>sasaQ</strong>
-            </div>
 
-            <div class="detail">
-                <p><strong>『sasaQ』</strong>は、社内向けの質問サービスです。<br/>
-                    新卒エンジニアとして働いてみて、チャットベースの質問では相手に自分の思いを伝えることが難しいと感じました。<br/>
-                    実際に、先輩に「何がわからないか伝わらない。マークダウン記法を使って質問するといい」とアドバイスをいただきました。<br/>
-                    質問をマークダウン記法を使って取りまとめることで、自分の伝えたいことが整理でき回答者側も的確なアドバイスができると思います。<br/>
-                    そういった実体験から、マークダウンの使える質問サービスを開発することにしました。<br/>
-                    また、社内向けのサービスのため質問や回答がデータとして蓄積していき、同じ質問で時間を取られることがなくなる点、<br/>
-                    マークダウン記法を使用することによる質問力の向上などの効果が期待できます。<br/>
-                    <strong>※テストログイン用のアカウントあります。ログインフォームにアクセスお願いいたします。</strong></p>
+        <div class="first-content">
+            <img src="{{asset( '/img/top2.png' )}}" alt="first-image">
+        </div>
+
+        <div class="second-content">
+            <div class="second-content-item">
+                <img class="second-content-image" src="{{asset( '/img/question.png' )}}" alt="question">
+                <p class="second-content-text">あああああああああああああああああああああああああ</p>
             </div>
-            <img class="persona-image" src="{{ asset('/img/ペルソナ.PNG') }}">
-            <p>※イメージ</p>
+            <div class="second-content-item">
+                <img class="second-content-image" src="{{asset( '/img/solution.png' )}}" alt="team">
+                <p class="second-content-text">あああああああああああああああああああああああああ</p>
+            </div>
         </div>
     </body>
 </html>

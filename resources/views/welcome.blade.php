2a57cac20239693f0069ece3d30ede600214e6c8
sasamario <mametaku3@yahoo.co.jp>
2020-12-07 22:21:34 +0900

    Fix：トップ画像の修正 Add：トップ画像上にキャッチコピー文の追加 Fix：セカンドコンテンツの文章の修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,17 +30,26 @@
         </div>
 
         <div class="first-content">
-            <img src="{{asset( '/img/top2.png' )}}" alt="first-image">
+            <img class="first-content-image" src="{{asset( '/img/top.png' )}}" alt="first-image">
+            <p class="first-content-text">「クールな質問をデータに」</p>
         </div>
 
         <div class="second-content">
-            <div class="second-content-item">
-                <img class="second-content-image" src="{{asset( '/img/question.png' )}}" alt="question">
-                <p class="second-content-text">あああああああああああああああああああああああああ</p>
+            <div class="second-content-item row">
+                <img class="second-content-image col-12 col-md-6" src="{{asset( '/img/question.png' )}}" alt="question">
+                <p class="second-content-text col-12 col-md-6"><strong>【課題】</strong><br/>
+                コロナウイルスの影響で、リモートワークが推奨されるようになりました。 その結果、対面での質問の機会が減り、チャットベースでの質問が基本となりました。<br/>
+                チャットベースでの質問になり以下の点が問題視されています。<br/><br/>
+                ◯チャットベースでの質問では自分の「わからないこと」「躓いていること」を伝えることが難しい。<br/><br/>
+                ◯個人チャットのようなクローズドなコミュニティでの質問になると他の人から見えないため同じような質問をされる可能性がある。</p>
             </div>
-            <div class="second-content-item">
-                <img class="second-content-image" src="{{asset( '/img/solution.png' )}}" alt="team">
-                <p class="second-content-text">あああああああああああああああああああああああああ</p>
+            <div class="second-content-item row">
+                <img class="second-content-image col-12 col-md-6" src="{{asset( '/img/solution.png' )}}" alt="team">
+                <p class="second-content-text col-12 col-md-6"><strong>【解決策】</strong><br/>
+                自分の考え等を伝えることが難しい件については、マークダウン記法を用いることで解決できる。<br/>
+                マークダウン記法を用いることで、見出しや強調など文字装飾が簡単にできるのため表現の幅が広がり伝えやすくなる。<br/><br/>
+                また、同じ質問を防ぐためにはオープンなコミュニティでかつ社内に質問と回答をデータとして残すことで解決できる。<br/>
+                上記の要件を満たすために、社内向け質問アプリケーションの開発を行いました。</p>
             </div>
         </div>
     </body>
